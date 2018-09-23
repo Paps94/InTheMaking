940c09059eaa00c6b3e5f24b786f4f37c727a451
Paps94 <antreas@OneHappyNoob.localdomain>
2018-09-23 09:13:36 +0100

    Testing of resume to locate error on pdf file

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 //To Download the resume
 Route::get('/download', function () {
   //PDF file is stored under public/resume/CV.pdf
-  $file= public_path(). "/resume/AntreasPapadopoulosCV.pdf";
+  $file= public_path(). "/resume/APCV.pdf";
 
   $headers = array(
             'Content-Type: application/pdf',
