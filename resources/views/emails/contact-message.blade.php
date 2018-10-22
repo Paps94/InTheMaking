5ef869113b7b13499fefd0ed12526626a4af273e
Paps94 <antreas@OneHappyNoob.localdomain>
2018-10-22 23:21:48 +0100

    Making changes to the email layout I receive after a successfull form is sent

--- resources/views/emails/contact-message.blade.php
+++ resources/views/emails/contact-message.blade.php
@@ -1,38 +1,30 @@
 @extends('layouts.app')
-
 @section('content')
 
-@component('mail::layout')
-    {{-- Header --}}
-    @slot('header')
-        @component('mail::header', ['url' => config('app.url')])
-            You got a new email from your website!
-        @endcomponent
-    @endslot
-
-{{-- Body --}}
-    Was sent by:  {{ $name }}
-    From :        {{ $email }}
-    Subject:      {{ $subject }}
-    _______________________________________________
-
-    {{ $bodyMessage }}
-
-{{-- Subcopy --}}
-    @isset($subcopy)
-        @slot('subcopy')
-            @component('mail::subcopy')
-                {{ $subcopy }}
-            @endcomponent
-        @endslot
-    @endisset
-
-{{-- Footer --}}
-    @slot('footer')
-        @component('mail::footer')
-            © {{ date('Y') }} {{ config('app.name') }}. Super FOOTER!
-        @endcomponent
-    @endslot
-@endcomponent
+<h3> Hey retard, somehow you have a contact request through your contact form</h3>
+<br>
+<h5>I am equally as surprised, yes.</h5>
+<br>
+<hr style="width: 100%;">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6" >
+       {{ $name }}
+    </div>
+    <div class="col-lg-6">
+       {{ $email }}
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12">
+      {{ $subject }}
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-12">
+      {{ $bodyMessage }}
+    </div>
+  </div>
+</div>
 
 @endsection
