512f8ba292a8335bedbb33f6b7d524a09997164a
Paps94 <antreas@OneHappyNoob.localdomain>
2018-10-23 23:16:48 +0100

    Changes to the email I receive when someone completes the Contact Form

--- resources/views/emails/contact-message.blade.php
+++ resources/views/emails/contact-message.blade.php
@@ -1,30 +1,37 @@
-@extends('layouts.app')
-@section('content')
+{{-- Header --}}
+   @slot('header')
+       @component('mail::header', ['url' => config('app.url')])
+           Guess who has a new mail!
+       @endcomponent
+   @endslot
 
-<h3> Hey retard, somehow you have a contact request through your contact form</h3>
-<br>
-<h5>I am equally as surprised, yes.</h5>
-<br>
-<hr style="width: 100%;">
-<div class="container">
-  <div class="row">
-    <div class="col-lg-6" >
-       {{ $name }}
-    </div>
-    <div class="col-lg-6">
-       {{ $email }}
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-lg-12">
-      {{ $subject }}
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-lg-12">
-      {{ $bodyMessage }}
-    </div>
-  </div>
-</div>
+{{-- Introduction --}}
+@component('mail::message')
+# You got a new email from your website. **How?!?**  Yes I am **equally as SURPRISED** as you!
 
-@endsection
+{{-- Body --}}
+@component('mail::table')
+--------------------------------------------------
+| Sender        | Email         | Subject        |
+| ------------- |:-------------:| --------:      |
+|  {{ $name }}  | {{ $email }}  | {{ $subject }} |
+--------------------------------------------------
+@endcomponent
+
+Email Content:   {{ $bodyMessage }}
+
+@component('mail::button', ['url' => 'inthemaking.tech'])
+Take me to my website
+@endcomponent
+
+Thanks,<br>
+Your **past self**
+
+{{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            © {{ date('Y') }} {{ config('app.name') }}. Super FOOTER!
+        @endcomponent
+    @endslot
+
+@endcomponent
